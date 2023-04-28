5542cba37cb54a6801ad68d53e10862052588a34
Henry Nguyen <43090951+rinanh102@users.noreply.github.com>
2023-04-28 16:28:46 +0700

    wip

--- index.ts
+++ index.ts
@@ -1,19 +1,26 @@
 import * as pdfUtil from 'pdf-to-text';
-var pdf_path = 'sample.pdf';
+import * as fs from 'fs';
+
+var pdf_path = 'BackhoeLoader.pdf';
 
 //Omit option to extract all text from the pdf file
 pdfUtil.pdfToText(pdf_path, function (err, data: string) {
   if (err) throw err;
 
   const searchResults = data.matchAll(
-    /^([A-Z, ]+)            [\W\w]+?\n\n[\W]+?Risk Treatments in Place:([\W\w]+?)\n([\W\w]+?)References:([\W\w]+?)\n/gim
+    /^([A-Z, ]+)(\b(?:CRITICAL|HIGH|MEDIUM|LOW)\b[\W\w]+?)\n[\W]+?Risk (?:Treatments in Place|Treatment Required):([\W\w]+?)\n([\W\w]+?)References:([\W\w]+?)\n/gim
   );
 
   for (const match of searchResults) {
-    console.log('title: ');
-    console.log(match[3]);
-    // console.log(match[2]);
-    // console.log(match[3]);
-    // console.log(match[4]);
-  }
+    console.log('match 1: ',match[1].trim());
+    console.log('match 2: ',match[2].trim());
+    console.log('match 3: ',match[3].trim());
+    console.log('match 4: ',match[4]);
+    console.log('match 5: ',match[5]);
+    // console.log('match 6: ',match[6]); 
+   }
+
+
 });
+
+//^\bTYPE\b([\W\w]+?)\n\bMAKE\b([\W\w]+?)\n\bMODEL\b([\W\w]+?)\n
